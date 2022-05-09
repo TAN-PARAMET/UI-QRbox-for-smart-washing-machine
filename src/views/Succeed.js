a3cef11291f509dd80cabd758082c9785f6538cf
Paramet chuchunchuen <zerooptv@gmail.com>
2022-05-09 11:03:06 +0700

    Update Succeed.js

--- src/views/Succeed.js
+++ src/views/Succeed.js
@@ -8,14 +8,7 @@ import animationData from "../lotties/correct.json";
 
 function Succeed() {
 
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  };
+  
 
   
 
@@ -23,11 +16,11 @@ function Succeed() {
   return (
     
     <div style={{ marginTop:'-30px', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', }}>
-      <Lottie  animationData={animationData}   style={{width:'300px',  }} />
+      <Lottie  animationData={animationData}   style={{width:'325px', marginBottom:'-50px',  }} />
     
 
     
-    <b className='linear' style={{ fontSize:'100px',  }}>ทำรายการสำเร็จ</b>
+    <b className='linear' style={{ fontSize:'80px',  }}>ทำรายการสำเร็จ</b>
     <p style={{ fontSize:'30px', }}>กรุณายืนรอ 5 วินาทีหรือกดปุ่มชำระเงินเพื่อใช้บริการ</p>
 
     
