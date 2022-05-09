bab7c12d10ee451c53bf0df6e8dc9c6079985362
Paramet chuchunchuen <zerooptv@gmail.com>
2022-05-09 10:27:40 +0700

    เมื่อเปิดจะไปหน้าhome

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './views/Home';
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link ,useNavigate} from "react-router-dom";
+import { useState, useEffect } from 'react'
 import Lottie from "lottie-react";
 
 
 
 
 function App() {
+  const navigate = useNavigate();
+useEffect(() => {
+  
+  navigate('/home')
+  return () => {
+    
+  }
+}, [])
+
+
+
   return (
     <div>
       <div className="App">
