c38b7aec9afa8181b4815db99182c41c92a8471e
Paramet chuchunchuen <zerooptv@gmail.com>
2022-05-09 12:11:30 +0700

    Update Succeed.js

--- src/views/Succeed.js
+++ src/views/Succeed.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet, Link } from "react-router-dom";
 import Lottie from 'lottie-react';
-import animationData from "../lotties/correct.json";
+import correct from "../lotties/correct.json";
 
 
 
@@ -16,12 +16,15 @@ function Succeed() {
   return (
     
     <div style={{ marginTop:'-30px', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', }}>
-      <Lottie  animationData={animationData}   style={{width:'325px', marginBottom:'-50px',  }}  />
+      <Lottie  animationData={correct}   style={{width:'325px', marginBottom:'-50px',  }}  />
     
 
     
     <b className='linear' style={{ fontSize:'80px',  }}>ทำรายการสำเร็จ</b>
     <p style={{ fontSize:'30px', marginTop:'-15px', }}>ขอบคุณที่ใช่บริการของพวกเรา</p>
+    <div className='background' style={{ margin: '20px auto', width: '400px', height: '70px', borderRadius: '100px', display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+            <b style={{ margin: 'auto', color: 'white', fontSize: '20px', }}>จะไปหน้าแจ้งเตือนในอีก 5 วินาที</b>
+          </div >
 
     
       
